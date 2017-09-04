591bc94118e4d9bc85bf45c88cedef32eea1e8a1
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-04 17:46:59 +0200

    Solved favourite number exercise

--- Exercises/1/favorite-number.js
+++ Exercises/1/favorite-number.js
@@ -1,5 +1,8 @@
 'use strict';
 
-var favoriteNumber = 8
+var favoriteNumber = 8;
 // Store your favorite number in a variable (as a number)
 // And print it like this: 'My favorite number is: 8'
+
+favoriteNumber = 7;
+console.log("My favorite number is: " + favoriteNumber);
