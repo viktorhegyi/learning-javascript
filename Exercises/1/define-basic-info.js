5ad8040bf4b81a32467ca5dd08866ac073fef394
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-05 13:38:29 +0200

    Solved define-basic-info exercise

--- Exercises/1/define-basic-info.js
+++ Exercises/1/define-basic-info.js
@@ -5,3 +5,13 @@
 // Your age as an integer
 // Your height in meters as a float
 // Whether you are married or not as a boolean
+
+var name = "Viktor";
+var age = 31;
+var height = 1.83;
+var married = true;
+
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(married);
