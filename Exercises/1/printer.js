93e756264671343b53fc60897a75da78e6bdfdcc
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-07 20:37:53 +0200

    Solved printer exercise

--- Exercises/1/printer.js
+++ Exercises/1/printer.js
@@ -2,3 +2,9 @@
 // - Create a function called `printer`
 //   which logs to the console the input parameters
 //   (can have multiple number of arguments)
+
+function printer(...args) {
+      console.log(args)
+}
+
+printer(1,2,3,4,'hello');
