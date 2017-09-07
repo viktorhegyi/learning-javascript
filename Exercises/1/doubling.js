baf77b7fbb137973c8879a95236dfe4cbe18293d
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-07 19:11:22 +0200

    Solved doubling exercise

--- Exercises/1/doubling.js
+++ Exercises/1/doubling.js
@@ -2,3 +2,12 @@
 // - Create a variable named `ak` and assign the value `123` to it
 // - Create a function called `doubling` that doubles it's input parameter
 // - Log the result of `doubling(ak)` to the console
+
+var ak = 123;
+
+function doubling(a) {
+  var result = a + a;
+  console.log(result);
+}
+
+doubling(ak);
