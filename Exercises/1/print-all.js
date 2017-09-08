125b55b120f5ca72948bf36a045375626dfcd274
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-08 21:04:50 +0200

    Solved print-all exercise

--- Exercises/1/print-all.js
+++ Exercises/1/print-all.js
@@ -3,3 +3,13 @@
 // - Log each the element of `af` to the console*
 // *hint: use a loop, console.log(af) won't cut it
 // - bonus for using the correct built in array method
+
+var af = [4, 5, 6, 7];
+
+for (var i = 0; i < af.length; i++) {
+  console.log(af[i]);
+}
+
+af.forEach(function(e) {
+  console.log(e);
+});
