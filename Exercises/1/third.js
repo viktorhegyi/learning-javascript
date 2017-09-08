5f71010d1097dae8ab49927ad9f6630e8ce3cc49
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-08 20:45:04 +0200

    Solved compare-length exercise

--- Exercises/1/third.js
+++ Exercises/1/third.js
@@ -2,3 +2,7 @@
 // - Create a variable named `magicNumbers`
 //   with the following content: `[1, 3, 5, 7]`
 // - Print the third element of `magicNumbers`
+
+var magicNumbers = [1, 3, 5, 7];
+
+console.log(magicNumbers[2]);
