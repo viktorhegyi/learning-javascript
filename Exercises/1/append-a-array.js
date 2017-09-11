00b9191c3b96c50c8515f891e85ddc51de95af58
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-11 22:40:17 +0200

    Solved append-a-array exercise

--- Exercises/1/append-a-array.js
+++ Exercises/1/append-a-array.js
@@ -3,3 +3,11 @@
 //   with the following content: `["kuty", "macs", "cic"]`
 // - Add all elements an `"a"` at the end
 // - try to use built in functions instead of loops
+
+var nimals = ['kuty', 'macs', 'cic'];
+
+var animals = nimals.map(function(number) {
+  return number += 'a';
+})
+
+console.log(animals);
