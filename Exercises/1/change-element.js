f77d8447aeeca33fa9260b05a39c4096bc4edf58
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-11 22:06:11 +0200

    Solved change-element exercise

--- Exercises/1/change-element.js
+++ Exercises/1/change-element.js
@@ -2,3 +2,9 @@
 // - Create an array named `s` with the following content: `[1, 2, 3, 8, 5, 6]`
 // - Change the 8 to 4 with the `.map` method
 // - Print the fourth element as a test
+
+var s = [1, 2, 3, 8, 5, 6];
+
+s[3] = 4;
+
+console.log(s);
