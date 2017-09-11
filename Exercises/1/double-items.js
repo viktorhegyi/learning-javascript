05cc1733dfa9c8652bd3fc4a815f2dfd260ca647
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-11 22:45:46 +0200

    Solved double-items exercise

--- Exercises/1/double-items.js
+++ Exercises/1/double-items.js
@@ -2,3 +2,11 @@
 // - Create an array variable named `ag` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
 // - Double all the strings in the array, use a built in array method instead of a loop
 // It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+var ag = ["Gin", "Whiskey", "Wine", "Beer"];
+
+var doubles = ag.map(function(number) {
+  return number + number;
+})
+
+console.log(doubles);
