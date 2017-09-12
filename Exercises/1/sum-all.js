cdea8c41c56ab0518970b6babc5f1d8a73959de7
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-12 18:45:57 +0200

    Solved sum-all exercise

--- Exercises/1/sum-all.js
+++ Exercises/1/sum-all.js
@@ -1,3 +1,10 @@
 'use strict';
 // - Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
 // - Log the sum of the elements in `ai` to the console
+
+var ai = [3, 4, 5, 6, 7];
+var result = ai.reduce(function(accumulator, currentValue) {
+    return accumulator + currentValue;
+});
+
+console.log(result);
