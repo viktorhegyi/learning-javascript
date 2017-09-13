09fc42dc95f46fc553a50cb861ca23b3fcc6bdb4
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-13 21:48:19 +0200

    Solved reverse string exercise

--- Exercises/1/reversestring.js
+++ Exercises/1/reversestring.js
@@ -2,3 +2,9 @@
 // Reverse the string!
 
 var reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+console.log(reverseString(reversed));
