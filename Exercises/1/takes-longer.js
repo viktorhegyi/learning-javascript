a3e81e91bfb17752bd9fb9e6247cf2d37d42d64a
Viktor Hegyi <viktorhegyi@gmail.com>
2017-09-13 22:02:09 +0200

    Solved takes-longer exercise

--- Exercises/1/takes-longer.js
+++ Exercises/1/takes-longer.js
@@ -4,4 +4,11 @@
 
 var quote = "Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law."
 
-console.log(quote);
+var index = quote.indexOf('It');
+var quoteFirsthalf = quote.slice(0, quote.indexOf('It') + 2);
+var quoteSecondhelf = quote.slice(quote.indexOf('It') + 2, quote.length);
+var adding = " always takes longer than";
+
+var newquote = quoteFirsthalf.concat(adding, quoteSecondhelf);
+
+console.log(newquote);
